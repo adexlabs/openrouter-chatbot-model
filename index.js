c1176384c4793cc149af7bec229da54fffd28719
adexlabs <44570524+adexlabs@users.noreply.github.com>
2026-06-01 15:43:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,8 +58,4 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+module.exports = app;
